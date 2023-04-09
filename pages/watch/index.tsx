2150f9a2e4faa71ff1171dc837d882420937a0b2
Thomas WITZ <86101452+SchauT@users.noreply.github.com>
2023-04-09 18:12:10 +0200

    Setup main page, add watch and create stream pages

---
+++ pages/watch/index.tsx
@@ -0,0 +1,13 @@
+import styles from '@/styles/Home.module.css'
+
+export default function CreateStream() {
+  return (
+    <>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          Watch page
+        </div>
+      </main>
+    </>
+  );
+}
